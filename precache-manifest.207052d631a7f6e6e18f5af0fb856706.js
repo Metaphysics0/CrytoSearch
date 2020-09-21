908da562de783be5f44eaf258460c1c4ebe5a355
Ryan Roberts <ryan@Ryans-MBP.fios-router.home>
2020-09-21 07:54:09 -0700

    Updates

---
+++ precache-manifest.207052d631a7f6e6e18f5af0fb856706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8720e8938f5308eccaec0b32697fe39",
+    "url": "/CryptoSearch/index.html"
+  },
+  {
+    "revision": "92441e2d9d0e9a146d34",
+    "url": "/CryptoSearch/static/css/main.c9b66865.chunk.css"
+  },
+  {
+    "revision": "e042156ecda685a4d548",
+    "url": "/CryptoSearch/static/js/2.86bab506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CryptoSearch/static/js/2.86bab506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92441e2d9d0e9a146d34",
+    "url": "/CryptoSearch/static/js/main.44d1b567.chunk.js"
+  },
+  {
+    "revision": "0af4774785c34d4360b1",
+    "url": "/CryptoSearch/static/js/runtime-main.d52e13fe.js"
+  }
+]);
